a048f5a5d466164e34269cac238df35595d423f6
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-10 18:25:34 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import LoginPage from './modules/LoginPage/LoginPage';
+import Dashboard from './modules/Dashboard/Dashboard';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <LoginPage />
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />
     }
   ]);
   return (
