f06b4e917a6375e8a6254f7cb919b907ee27fbe6
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-10 18:23:38 +0530

    update

--- frontend/src/modules/Dashboard/Dashboard.jsx
+++ frontend/src/modules/Dashboard/Dashboard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './Dashboard.module.css'
 
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    return (
+        <div>Dashboard</div>
+    )
 }
 
 export default Dashboard
