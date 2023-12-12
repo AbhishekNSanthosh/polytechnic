8b9a6e09796e81b7cfb1ffc6623e33f4e33d0a95
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-12 21:56:05 +0530

    major ui updates

---
+++ frontend/src/modules/Management/Management.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Management.module.css'
+
+const Management = () => {
+    return (
+        <div>Management</div>
+    )
+}
+
+export default Management
