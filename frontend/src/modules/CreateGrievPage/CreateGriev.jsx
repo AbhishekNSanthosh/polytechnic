071889a6d2c0a489d3502e49a207d17107ac8f96
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-17 17:24:43 +0530

    update

--- frontend/src/modules/CreateGrievPage/CreateGriev.jsx
+++ frontend/src/modules/CreateGrievPage/CreateGriev.jsx
@@ -3,7 +3,25 @@ import styles from './CreateGriev.module.css'
 
 const CreateGriev = () => {
     return (
-        <div>CreateGriev</div>
+        <div className={styles.container}>
+            <div className={styles.wrap}>
+                <div className={styles.topRow}>
+                    <span className={styles.title}>Create Grievance</span>
+                </div>
+                <div className={styles.grievBox}>
+                    <div className={styles.grievBoxRow}>
+                        <input type="text" className="" />
+                    </div>
+                    <div className={styles.grievBoxRow}>
+                        <input type="text" className="" />
+                    </div>
+                    <div className={styles.grievBoxRow}>
+                        <button className=""></button>
+                        <button className=""></button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
