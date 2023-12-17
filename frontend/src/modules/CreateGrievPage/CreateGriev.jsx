2f0bbccb788fd6053e653912b98f68981bb882fb
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-17 17:00:14 +0530

    update

--- frontend/src/modules/CreateGrievPage/CreateGriev.jsx
+++ frontend/src/modules/CreateGrievPage/CreateGriev.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './CreateGriev.module.css'
 
 const CreateGriev = () => {
-  return (
-    <div>CreateGriev</div>
-  )
+    return (
+        <div>CreateGriev</div>
+    )
 }
 
 export default CreateGriev
