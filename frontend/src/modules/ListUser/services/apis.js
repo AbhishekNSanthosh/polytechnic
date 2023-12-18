bb52df3fb6a33d54653af9912e78884d20680f2f
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-18 23:34:23 +0530

    major-updates

--- frontend/src/modules/ListUser/services/apis.js
+++ frontend/src/modules/ListUser/services/apis.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 import { adminApi, backendApiUrl } from '../../../utils/helpers'
 
 export const getUsersByAdmin = async (
+    semester,
+    department,
+    role,
     authToken
 ) => {
     try {
         const response = await axios.get(backendApiUrl + adminApi.getAllUsers, {
+
+        },{
             headers: {
                 Authorization: "Bearer " + authToken
             }
