01d8a70f22809cfb3440178d5795ec04b3bf80be
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-21 22:43:34 +0530

    manage letter

--- frontend/src/modules/ManageLetter/ManageLetter.jsx
+++ frontend/src/modules/ManageLetter/ManageLetter.jsx
@@ -3,7 +3,18 @@ import styles from './ManageLetter.module.css'
 
 const ManageLetter = () => {
     return (
-        <div>ManageLetter</div>
+        <div className={styles.container}>
+            <div className={styles.wrap}>
+                <div className={styles.titleRow}>
+                    <span className={styles.title}>Manage Letter</span>
+                </div>
+                <div className={styles.manageRow}>
+                    <div className={styles.manageItemTitle}>
+                        <span className={styles.itemtitle}>Manage View Access Permission :</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
