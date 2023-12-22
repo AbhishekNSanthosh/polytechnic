5b7997ddd15b66b5c7fdc290441f4a6e908ef919
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-22 20:52:45 +0530

    changes in manage-letter

--- frontend/src/modules/ManageLetter/services/apis.js
+++ frontend/src/modules/ManageLetter/services/apis.js
@@ -104,3 +104,30 @@ export const searchUser = async (
         }
     }
 }
+
+export const updateAccess = async () => {
+    try {
+
+    } catch (error) {
+        toast({
+            title: error?.response?.data?.message,
+            // description: "Redirecting to Login page",
+            status: 'error',
+            duration: 3000,
+            isClosable: true,
+        });
+        if (error?.response?.data?.error === "TokenExpiredError") {
+            toast({
+                title: 'Session Expired',
+                description: "Redirecting to Login page",
+                status: 'error',
+                duration: 3000,
+                isClosable: true,
+            });
+            localStorage.clear();
+            setTimeout(() => {
+                navigate('/')
+            }, 2000);
+        }
+    }
+}
