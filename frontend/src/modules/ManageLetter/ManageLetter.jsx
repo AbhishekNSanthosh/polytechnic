cd0b69f8cdebad566c48281f1c9d93fb907c18ed
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-22 08:13:10 +0530

    manage letter

--- frontend/src/modules/ManageLetter/ManageLetter.jsx
+++ frontend/src/modules/ManageLetter/ManageLetter.jsx
@@ -12,6 +12,20 @@ const ManageLetter = () => {
                     <div className={styles.manageItemTitle}>
                         <span className={styles.itemtitle}>Manage View Access Permission :</span>
                     </div>
+                    <div className={styles.manageItemRow}>
+                        <div className={styles.manageItemLeft}>
+                            <div className={styles.manageItemTop}>
+                                <div className={styles.searchBox}>
+                                    <input type="text" placeholder='Search faculty' className={styles.search}/>
+                                </div>
+                                <div className={styles.manageTopActions}>
+                                    <input type="text" className={styles.search}/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.verticalLine}></div>
+                        <div className={styles.manageItemRight}>hi</div>
+                    </div>
                 </div>
             </div>
         </div>
