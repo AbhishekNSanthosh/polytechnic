226faa1a8007a40cad11873dee019630c223b6b1
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2023-12-27 06:50:41 +0530

    download letter

---
+++ frontend/src/modules/Download/Download.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Download.module.css'
+
+const Download = () => {
+  return (
+    <div className={styles.container}>Download</div>
+  )
+}
+
+export default Download
