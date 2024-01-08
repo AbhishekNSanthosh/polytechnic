2f2b30ea7f1bd4da9a86bc602dc89ab2905cf517
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-01-08 20:59:39 +0530

    hot-fix

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,8 @@ const { resMessages } = require('./Utils/Helpers.js');
 dotenv.config();
 const app = express();
 // app.use(limiter);
+app.enable('trust proxy');
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
@@ -22,9 +24,15 @@ app.use("/api/v2/admin", adminRoute);
 app.use("/api/v2/student", studentRoute);
 app.use("/api/v2/teacher", teacherRoute);
 app.use("/api/v2/public", publicRoute);
-
+app.use((req, res, next) => {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
