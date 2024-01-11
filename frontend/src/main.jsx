14cf615b5b50cf1332754ae345d3993ae992d714
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-01-11 20:58:05 +0530

    meta

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
 import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
