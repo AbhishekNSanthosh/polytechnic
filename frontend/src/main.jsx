b90ff829491abb6c42dc48d9611a1f97c7d2e35a
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-01-11 20:45:20 +0530

    meta

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <ChakraProvider>
-    <Toaster />
+      <Toaster />
       <App />
     </ChakraProvider>
-  // </React.StrictMode>,
+  </React.StrictMode>
 )
