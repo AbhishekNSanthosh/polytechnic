17473b944effc0547dc8696dcb7ea0a6ba207cb8
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-01-19 19:41:52 +0530

    optimization

--- frontend/src/modules/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/modules/ForgotPassword/ForgotPassword.jsx
@@ -27,7 +27,7 @@ const ForgotPassword = () => {
     }
     return (
         <div className={styles.container}>
-            <img src={collegeImages.collegelogosvg} alt="" className={styles.logo} />
+            <img  loading='lazy' src={collegeImages.collegelogosvg} alt="" className={styles.logo} />
             <div className={styles.forgotBox}>
                 <span className={styles.title}>Forgot your Password ?</span>
                 <span className={styles.desc}>Please enter the email address which you'd like your password reset information sent to </span>
